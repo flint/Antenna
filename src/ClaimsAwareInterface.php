34df2105354d7b80e10879215303f90ec5264eb1
Henrik Bjornskov <hb@peytz.dk>
2015-09-06 12:48:59 +0200

    Add LICENSE information to files fixes #6

--- src/ClaimsAwareInterface.php
+++ src/ClaimsAwareInterface.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * This file is part of the Antenna package.
+ *
+ * (c) 2015 Henrik Bjornskov <henrik@bjrnskov.dk>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
 namespace Antenna;
 
 interface ClaimsAwareInterface
